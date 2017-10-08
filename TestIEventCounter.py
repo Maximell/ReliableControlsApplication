f3ddf3b8937abe61578f3d3ebe7baf85f1ac6dee
Maxim Ellison <ellisonmaxim@gmail.com>
2017-10-08 08:46:06 -0700

    added a few more test cases

--- TestIEventCounter.py
+++ TestIEventCounter.py
@@ -16,5 +16,17 @@ class TestIEventCounter(unittest.TestCase):
         eventCounter.parseEvents("testDevice2", f)
         self.assertEqual(2, eventCounter.getEventCount("testDevice2"))
 
+    def testInput3(self):
+        eventCounter = IEventCounter()
+        f = open('./TestInputs/input3.csv', 'r')
+        eventCounter.parseEvents("testDevice3", f)
+        self.assertEqual(0, eventCounter.getEventCount("testDevice3"))
+
+    def testInput4(self):
+        eventCounter = IEventCounter()
+        f = open('./TestInputs/input4.csv', 'r')
+        eventCounter.parseEvents("testDevice4", f)
+        self.assertEqual(1, eventCounter.getEventCount("testDevice4"))
+
 if __name__ == '__main__':
     unittest.main()
